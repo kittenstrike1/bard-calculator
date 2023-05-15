07b3979d5a7f3dc8d57138de5fb3c8e44034674e
Kitten Strike <90493410+notrealkittenstrike@users.noreply.github.com>
2023-05-15 21:34:04 +0700

    Add files via upload

---
+++ bye hav a good time.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>bai hav a good time</title>
+    <script>
+        alert('bye hav a good time :)');
+    </script>
+</head>
+<body>
+</body>
+</html>
